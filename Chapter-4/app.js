eee0775080478cd634475d8e09000e3dccec1428
Yasrab Memon <yasrab7memon@gmail.com>
2020-06-14 17:38:50 -0700

    Fisrt commit

---
+++ Chapter-4/app.js
@@ -0,0 +1,27 @@
+// //Task-1
+// var var1 ,var2 ,var3;
+
+// //Task-2
+// //legal
+// var name1;
+// var name_1;
+// var ali_mughal;
+// var yasrabMemon;
+// var $yasrab;
+
+// //Task-3
+// //illegal
+// // var 1name;
+// // var a&b;
+// // var name-1;
+// // var %name;
+// // var name 1;
+
+
+// //Task-4
+// document.write("<h1>Rules for naming JS variables</h1><br>");
+// document.write("Variable names can only contain letter, numbers, dollar signs and underscores. For example <b>$my_1stVariable</b><br>");
+// document.write("Variables must begin with a letter, $ and _. For example: <b> $name, _name or name.</b><br>");
+// document.write("Variable names are case sensitive.<br>");
+// document.write("Variable names should not be JS keywords.<br>");
+
